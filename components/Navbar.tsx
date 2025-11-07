0ff60454e40dede15469ab1cbb634cd70ce7bd35
itskvsin <kevinsolankistudy@gmail.com>
2025-11-07 11:24:08 +0530

    modified the navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -6,9 +6,9 @@ import { motion } from "framer-motion";
 export default function Navbar() {
   return (
     <section>
-      <div className="border-b border-white flex items-center bg-black/60 justify-between fixed z-50 w-screen px-10 lg:px-30">
+      <div className="border-b border-white flex items-center bg-black/60 justify-between fixed z-50 w-screen px-8 lg:px-30">
         <div>
-          <h1 className="font-bold text-xl lg:text-3xl text-white font-bakbak">YNOTNOW</h1>
+          <h1 className="font-bold text-xl lg:text-3xl text-white font-bakbak w-1/4 lg:w-2/4">YNOTNOW</h1>
         </div>
 
         <motion.div
@@ -28,7 +28,7 @@ export default function Navbar() {
           />
         </motion.div>
 
-        <div className="w-1/10"></div>
+        <div className="w-1/6 lg:w-1/10"></div>
       </div>
     </section>
   );
