8e0f1e7e712338e8557512497d643f9494d3c2cc
Kevin Solanki <142715013+itskvsin@users.noreply.github.com>
2025-11-12 16:57:48 +0530

    Update mobile image source in Hero component

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -23,7 +23,7 @@ export default function Hero() {
           className="object-cover lg:object-cover hidden md:flex lg:flex  transition-opacity duration-1000"
         />
         <Image
-          src="/images/mobileImgChange.png"
+          src="/images/changeImgMobile.png"
           alt="Background image"
           fill
           priority
